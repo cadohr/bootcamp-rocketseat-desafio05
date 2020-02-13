94b6fa93b6cc719aac8979cd5c6c13d023e45451
Ricardo Jacinto <cadohr@gmail.com>
2020-02-13 11:55:30 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Desafio 05: Primeiro Projeto com ReactJS
+
+<h1 align="center">
+    <img alt="GoStack" src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/bootcamp-header.png" width="200px" />
+</h1>
+
+## About
+
+A simple react app to add repositories to a list saved on local storage and see repository details, filtering by issues and paginate.
+
+## Installation
+
+Clone the project with
+
+```sh
+git clone https://github.com/cadohr/rocketseat-bootcamp-desafio05.git
+```
+
+Get in the project path
+
+```sh
+cd rocketseat-bootcamp-desafio05
+```
+
+Then install the dependencies with:
+
+```sh
+yarn
+```
+
+Start application with:
+
+```sh
+yarn start
+```
+
+Open browser on:
+
+```sh
+localhost:3000
+```
+
+## Screenshot
+
+<img alt="Screenshot Main" src=".github/screenshot-main.png" />
---
+++ src/pages/Main/index.js
@@ -0,0 +1,108 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+
+import api from '../../services/api';
+
+import Container from '../../components/Container';
+import { Form, Input, SubmitButton, List } from './styles';
+
+class Main extends Component {
+  state = {
+    newRepo: '',
+    repositories: [],
+    loading: false,
+    error: false,
+  };
+
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+
+    if (repositories) {
+      this.setState({ repositories: JSON.parse(repositories) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value, error: false });
+  };
+
+  handleSubmit = async e => {
+    try {
+      e.preventDefault();
+
+      this.setState({ loading: true });
+
+      const { newRepo, repositories } = this.state;
+
+      if (
+        repositories.find(
+          repo => repo.name.toLowerCase() === newRepo.toLowerCase()
+        )
+      ) {
+        throw new Error('Repositório duplicado');
+      }
+      const response = await api.get(`/repos/${newRepo}`);
+
+      const data = { name: response.data.full_name };
+
+      this.setState({
+        repositories: [...repositories, data],
+        newRepo: '',
+        loading: false,
+      });
+    } catch (err) {
+      this.setState({ error: true, loading: false });
+    }
+  };
+
+  render() {
+    const { newRepo, loading, repositories, error } = this.state;
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <Input
+            type="text"
+            placeholder="Adicionar repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+            error={error}
+          />
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#fff" size={14} />
+            ) : (
+              <FaPlus color="#fff" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+
+        <List>
+          {repositories.map(repository => (
+            <li key={repository.name}>
+              <span>{repository.name}</span>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
+            </li>
+          ))}
+        </List>
+      </Container>
+    );
+  }
+}
+
+export default Main;
